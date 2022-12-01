9ceb78c6c01fc0de78d744ff9caa9d3f0550835e
GaraFox <47352662+GaraFox@users.noreply.github.com>
2022-11-30 20:54:41 -0800

    new file:   167. Two Sum II - Input Array Is Sorted [TLE].c

---
+++ 167. Two Sum II - Input Array Is Sorted [TLE].c
@@ -0,0 +1,44 @@
+// Time Limit Exceeded
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
+    int pt1 = 0;
+    int pt2;
+    int temp1;
+    int temp2;
+    int isBreak = 0;
+    
+    while(pt1 < numbersSize - 1){
+       // temp1 = numbers[pt1];
+        pt2 = pt1 + 1;
+        
+        while(pt2 < numbersSize){
+           // temp2 = numbers[pt2];
+            if(numbers[pt1] + numbers[pt2] == target){
+                isBreak = 1;
+                break;
+            }
+            pt2++;
+        }
+        
+        if(isBreak == 1){
+            break;
+        }
+        
+        pt1++;
+        
+    }
+    
+    pt1++;
+    pt2++;
+    
+    *returnSize = 2;
+    int* out = (int*)malloc(*returnSize*sizeof(int));
+    
+    out[0] = pt1;
+    out[1] = pt2;
+    
+    return out;
+}
